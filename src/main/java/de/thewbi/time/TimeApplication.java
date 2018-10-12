d8f31283343697dac7a36e819c95c4008c1bd526
wolfgang bischoff <wfb2@gmx.de>
2018-10-12 18:46:36 +0200

    Initial commit

---
+++ src/main/java/de/thewbi/time/TimeApplication.java
@@ -0,0 +1,12 @@
+package de.thewbi.time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeApplication.class, args);
+	}
+}
